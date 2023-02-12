7608490dabd6ac3a064017d512ca756a041745cd
jorge antonio <jorgeantonio.acll@gmail.com>
2023-02-12 13:06:41 -0300

    Abstracao e Encapsulamento

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+      Curso curso1 =new Curso();
+      curso1.setTitulo("curso Java");
+      curso1.setDescricao("descriçao curso java");
+      curso1.setCargaHoraria(8);
+      System.out.println(curso1);
+      Curso curso2 =new Curso();
+      curso2.setTitulo("curso html");
+      curso2.setDescricao("descriçao curso html");
+      curso2.setCargaHoraria(4);
+      System.out.println(curso2);
+
+      Mentoria mentoria1 = new Mentoria();
+      mentoria1.setTitulo("Mentoria java");
+      mentoria1.setDescricao("Ensinado a melhorar no java");
+      mentoria1.setData(LocalDate.now());
+      System.out.println(mentoria1);
+    }
+}
